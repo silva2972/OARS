ae1320f1e8ffe2597e2bc242874acd396c4c1cea
silva2972 <silva2972@gmail.com>
2016-08-17 20:44:02 -0500

    deleted script.js

--- src/oars/wwwroot/_references.js
+++ src/oars/wwwroot/_references.js
@@ -1,5 +1,4 @@
 ﻿/// <autosync enabled="true" />
-/// <reference path="js/javascript.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js" />
